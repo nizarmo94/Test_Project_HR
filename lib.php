a8f2b977cf43e612c518a8fd66bc4405b78607d0
Nizar_Mohamed_Ahmed <95687716+nizarmo94@users.noreply.github.com>
2021-12-07 17:02:16 +0200

    Update lib.php

--- lib.php
+++ lib.php
@@ -1,9 +1,7 @@
 <?php
 session_start();
 $con = mysqli_connect("localhost", "root", "", "tes") or die("!!!!!!!!!!!!!!");
-
 mysqli_query($con, "set names utf8");
-
 header('Content-Type: text/html; charset=utf-8');
 function result($con, $q)
 {
@@ -12,7 +10,6 @@ function result($con, $q)
     else
         return "";
 }
-
 if (isset($_SESSION['username']) and isset($_SESSION['password']) and isset($_SESSION['type'])) {
     $username =  $_SESSION['username'];
     $password = $_SESSION['password'];
@@ -32,12 +29,10 @@ if (isset($_SESSION['username']) and isset($_SESSION['password']) and isset($_SE
         font-family: khalid art;
         src: url(Font_Sys/Khalid-Art-bold.ttf);
     }
-
     @font-face {
         font-family: adobe arabic;
         src: url(Font_Sys/AdobeArabic-bold.otf);
     }
-
     h2 {
         font-family: khalid art;
     }
